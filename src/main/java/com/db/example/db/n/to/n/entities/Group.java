e35abd088eef4eab69b8aed1a2386ae1baedf848
Ocean Liang <657175454@qq.com>
2018-07-27 01:54:58 +0800

    create Group and User entities

--- src/main/java/com/db/example/db/n/to/n/entities/Group.java
+++ src/main/java/com/db/example/db/n/to/n/entities/Group.java
@@ -1,4 +1,48 @@
 package com.db.example.db.n.to.n.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "groupList")
+    private List<User> userList;
+
+    public Group() {
+    }
+
+    public Group(String name, List<User> userList) {
+        this.name = name;
+        this.userList = userList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
 }
--- src/main/java/com/db/example/db/n/to/n/entities/User.java
+++ src/main/java/com/db/example/db/n/to/n/entities/User.java
@@ -1,4 +1,50 @@
 package com.db.example.db.n.to.n.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "user_group",joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id"))
+    private List<Group> groupList;
+
+    public User() {
+    }
+
+    public User(String name, List<Group> groupList) {
+        this.name = name;
+        this.groupList = groupList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Group> getGroupList() {
+        return groupList;
+    }
+
+    public void setGroupList(List<Group> groupList) {
+        this.groupList = groupList;
+    }
 }
