6c9d23e018d5626ed1a310277b740526d4dc8cc9
Ocean Liang <657175454@qq.com>
2018-07-27 00:57:52 +0800

    update leader to klass

--- src/main/java/com/db/example/db/one/to/one/controllers/KlassController.java
+++ src/main/java/com/db/example/db/one/to/one/controllers/KlassController.java
@@ -1,12 +1,15 @@
 package com.db.example.db.one.to.one.controllers;
 
+import com.db.example.db.one.to.n.entities.Company;
 import com.db.example.db.one.to.one.dto.KlassDTO;
 import com.db.example.db.one.to.one.entities.Klass;
 import com.db.example.db.one.to.one.entities.Leader;
 import com.db.example.db.one.to.one.repositories.KlassRepository;
 import com.db.example.db.one.to.one.repositories.LeaderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -46,5 +49,18 @@ public class KlassController {
         Klass one = klassRepository.findById(id).get();
         if(one == null) return null;
         return new KlassDTO(one);
+    }
+
+    @Transactional
+    @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity update(@PathVariable Long id,@RequestBody Leader leader) {
+        long a = id;
+        Klass klass = klassRepository.findById(id).get();
+        klass.getLeader().setKlass(null);
+        klass.setLeader(leader);
+        leader.setKlass(klass);
+        leaderRepository.save(leader);
+        klassRepository.save(klass);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
