2c5b08fd85a186ea368d0286e7c8e9ca8064b303
Ocean Liang <657175454@qq.com>
2018-07-27 09:30:22 +0800

    update sql table

--- src/main/java/com/db/example/db/n/to/n/entities/Group.java
+++ src/main/java/com/db/example/db/n/to/n/entities/Group.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Table(name = "w_group")
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/resources/db/migration/V7__Create_USER_GROUP_Table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `user_group`(
+  `user_id` BIGINT NOT NULL,
+  `group_id` BIGINT NOT NULL
+);
+
